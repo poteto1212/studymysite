c165af17e4f2eb2598b5870fc35912d537a103ac
Your Name <you@example.com>
2021-06-27 09:03:15 +0000

    init

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+#モデルからの読み込み
+from .models import Grade,Member
+
+#gradeに対する操作権限
+admin.site.register(Grade)
+
+#Memberに対する操作権限
+admin.site.register(Member)
 
 # Register your models here.
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+#学年モデル
+class Grade(models.Model):
+    #学年の列に当たる
+    grades=models.CharField(verbose_name="学年一覧",max_length=10)
+    
+    #おまじない
+    def __str__(self):
+        return self.grades
+    
+    
+    #表に名前を付ける
+    class Meta:
+        verbose_name_plural="学年モデル"
+    
+
+
+#メンバーモデル
+class Member(models.Model):
+   #学年選択
+    grade=models.ForeignKey('Grade',verbose_name="学年を選択してください",on_delete=models.CASCADE)
+    #部員名
+    name=models.CharField(verbose_name="名前",max_length=25)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural="メンバーモデル"
